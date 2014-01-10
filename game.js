cb9df1892c345468198cc62915654e6316126cf0
vagrant <vagrant@lighthouse-labs>
2014-01-10 00:23:39 +0000

    Created JS file

---
+++ game.js
@@ -0,0 +1,12 @@
+var randomNumber1 = Math.floor((Math.random()*20) + 1);
+var randomNumber2 = Math.floor((Math.random()*20) + 1);
+
+
+var equation = prompt("What is " + randomNumber1 + "+" + randomNumber2 + " ?");
+
+
+
+
+
+
+
